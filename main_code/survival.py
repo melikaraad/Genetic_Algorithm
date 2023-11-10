65745daf486544225791cb3afe2ec2e853cd01ba
melikaraad <124432068+melikaraad@users.noreply.github.com>
2023-11-10 10:42:50 +0330

    Create survival.py

---
+++ main_code/survival.py
@@ -0,0 +1,15 @@
+from Genetic_Algorithm import n_population, population
+from recombination import offspring
+
+def survival(population,offspring,n_population):
+    mymin = float('inf')
+    index = 0
+    for i in [0,n_population - 1]:
+        if population[i].fitness < mymin:
+            index = i
+            mymin = population[i].fitness
+    
+    population[index] = offspring
+    e=population
+
+    return e
